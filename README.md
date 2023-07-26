de12094535ef590ec4d1f13bed2882a84c2b565a
Cornslakes <steffen.weiffenbach@gmx.de>
2023-07-26 19:41:04 +0200

    readme and web-app navbar fix

--- README.md
+++ README.md
@@ -3,24 +3,28 @@
 ## Installation
 
 Install necessary packages:
+
 ```
 sudo apt update
 sudo apt install -y apt-transport-https wget gpg
 ```
 
 Install docker:
+
 ```
-sudo apt install -y docker.io 
+sudo apt install -y docker.io
 sudo usermod -aG docker $USER && newgrp docker
 ```
 
 Install minikube:
+
 ```
 wget https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 ```
 
 Install kubectl:
+
 ```
 wget -O - https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -29,6 +33,7 @@ sudo apt install -y kubectl
 ```
 
 Install Helm:
+
 ```
 wget -O - https://baltocdn.com/helm/signing.asc | sudo gpg --dearmor -o /etc/apt/keyrings/helm.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
@@ -37,11 +42,13 @@ sudo apt install helm
 ```
 
 Start minikube:
+
 ```
 minikube start --cpus=4 --memory=8000MB --addons=ingress
 ```
 
 Install Strimzi Operator and Kafka Cluster:
+
 ```
 helm repo add strimzi http://strimzi.io/charts/
 helm install my-kafka-operator strimzi/strimzi-kafka-operator
@@ -49,12 +56,14 @@ kubectl apply -f https://farberg.de/talks/big-data/code/helm-kafka-operator/kafk
 ```
 
 Install Hadoop:
+
 ```
 helm repo add pfisterer-hadoop https://pfisterer.github.io/apache-hadoop-helm/
 helm install my-hadoop-cluster pfisterer-hadoop/hadoop --set hdfs.dataNode.replicas=1 --set yarn.nodeManager.replicas=1
 ```
 
 Install Skaffold:
+
 ```
 wget -O skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
 sudo install skaffold /usr/local/bin/
@@ -62,6 +71,14 @@ sudo install skaffold /usr/local/bin/
 
 ## Deploy
 
+# For linux machines
+
 The application can be started using `skaffold dev`.
 To access the application, visit `http://<IP OF MINIKUBE>/`.
 The IP of minikube can be queried using `minikube ip`.
+
+# For WSL users
+
+The application can be started using `skaffold dev`.
+To expose the application outside your VM use `minikube service popular-slides-service --url`.
+The IP of the application can be accesses in your browser `http://localhost:<PORT OF APPLICATION>`
--- web-app/index.js
+++ web-app/index.js
@@ -1,66 +1,85 @@
-const os = require('os')
-const dns = require('dns').promises
-const { program: optionparser } = require('commander')
-const { Kafka } = require('kafkajs')
-const mariadb = require('mariadb')
-const MemcachePlus = require('memcache-plus')
-const express = require('express')
+const os = require("os");
+const dns = require("dns").promises;
+const { program: optionparser } = require("commander");
+const { Kafka } = require("kafkajs");
+const mariadb = require("mariadb");
+const MemcachePlus = require("memcache-plus");
+const express = require("express");
 
-const app = express()
-const cacheTimeSecs = 15
-const numberOfProjects = 30
+const app = express();
+const cacheTimeSecs = 15;
+const numberOfProjects = 30;
 
 // -------------------------------------------------------
 // Command-line options (with sensible defaults)
 // -------------------------------------------------------
 
 let options = optionparser
-	.storeOptionsAsProperties(true)
-	// Web server
-	.option('--port <port>', "Web server port", 3000)
-	// Kafka options
-	.option('--kafka-broker <host:port>', "Kafka bootstrap host:port", "my-cluster-kafka-bootstrap:9092")
-	.option('--kafka-topic-tracking <topic>', "Kafka topic to tracking data send to", "tracking-data")
-	.option('--kafka-client-id < id > ', "Kafka client ID", "tracker-" + Math.floor(Math.random() * 100000))
-	// Memcached options
-	.option('--memcached-hostname <hostname>', 'Memcached hostname (may resolve to multiple IPs)', 'my-memcached-service')
-	.option('--memcached-port <port>', 'Memcached port', 11211)
-	.option('--memcached-update-interval <ms>', 'Interval to query DNS for memcached IPs', 5000)
-	// Database options
-	.option('--mariadb-host <host>', 'MariaDB host', 'my-app-mariadb-service')
-	.option('--mariadb-port <port>', 'MariaDB port', 3306)
-	.option('--mariadb-schema <db>', 'MariaDB Schema/database', 'popular')
-	.option('--mariadb-username <username>', 'MariaDB username', 'root')
-	.option('--mariadb-password <password>', 'MariaDB password', 'mysecretpw')
-	// Misc
-	.addHelpCommand()
-	.parse()
-	.opts()
+  .storeOptionsAsProperties(true)
+  // Web server
+  .option("--port <port>", "Web server port", 3000)
+  // Kafka options
+  .option(
+    "--kafka-broker <host:port>",
+    "Kafka bootstrap host:port",
+    "my-cluster-kafka-bootstrap:9092"
+  )
+  .option(
+    "--kafka-topic-tracking <topic>",
+    "Kafka topic to tracking data send to",
+    "tracking-data"
+  )
+  .option(
+    "--kafka-client-id < id > ",
+    "Kafka client ID",
+    "tracker-" + Math.floor(Math.random() * 100000)
+  )
+  // Memcached options
+  .option(
+    "--memcached-hostname <hostname>",
+    "Memcached hostname (may resolve to multiple IPs)",
+    "my-memcached-service"
+  )
+  .option("--memcached-port <port>", "Memcached port", 11211)
+  .option(
+    "--memcached-update-interval <ms>",
+    "Interval to query DNS for memcached IPs",
+    5000
+  )
+  // Database options
+  .option("--mariadb-host <host>", "MariaDB host", "my-app-mariadb-service")
+  .option("--mariadb-port <port>", "MariaDB port", 3306)
+  .option("--mariadb-schema <db>", "MariaDB Schema/database", "popular")
+  .option("--mariadb-username <username>", "MariaDB username", "root")
+  .option("--mariadb-password <password>", "MariaDB password", "mysecretpw")
+  // Misc
+  .addHelpCommand()
+  .parse()
+  .opts();
 
 // -------------------------------------------------------
 // Database Configuration
 // -------------------------------------------------------
 
 const pool = mariadb.createPool({
-	host: options.mariadbHost,
-	port: options.mariadbPort,
-	database: options.mariadbSchema,
-	user: options.mariadbUsername,
-	password: options.mariadbPassword,
-	connectionLimit: 5
-})
+  host: options.mariadbHost,
+  port: options.mariadbPort,
+  database: options.mariadbSchema,
+  user: options.mariadbUsername,
+  password: options.mariadbPassword,
+  connectionLimit: 5,
+});
 
 async function executeQuery(query, data) {
-	let connection
-	try {
-		connection = await pool.getConnection()
-		console.log("Executing query ", query)
-		let res = await connection.query({ rowsAsArray: true, sql: query }, data)
-		return res
-	} finally {
-		if (connection)
-			connection.end()
-	}
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    console.log("Executing query ", query);
+    let res = await connection.query({ rowsAsArray: true, sql: query }, data);
+    return res;
+  } finally {
+    if (connection) connection.end();
+  }
 }
 
 // -------------------------------------------------------
@@ -68,45 +87,53 @@ async function executeQuery(query, data) {
 // -------------------------------------------------------
 
 //Connect to the memcached instances
-let memcached = null
-let memcachedServers = []
+let memcached = null;
+let memcachedServers = [];
 
 async function getMemcachedServersFromDns() {
-	try {
-		// Query all IP addresses for this hostname
-		let queryResult = await dns.lookup(options.memcachedHostname, { all: true })
+  try {
+    // Query all IP addresses for this hostname
+    let queryResult = await dns.lookup(options.memcachedHostname, {
+      all: true,
+    });
 
-		// Create IP:Port mappings
-		let servers = queryResult.map(el => el.address + ":" + options.memcachedPort)
+    // Create IP:Port mappings
+    let servers = queryResult.map(
+      (el) => el.address + ":" + options.memcachedPort
+    );
 
-		// Check if the list of servers has changed
-		// and only create a new object if the server list has changed
-		if (memcachedServers.sort().toString() !== servers.sort().toString()) {
-			console.log("Updated memcached server list to ", servers)
-			memcachedServers = servers
+    // Check if the list of servers has changed
+    // and only create a new object if the server list has changed
+    if (memcachedServers.sort().toString() !== servers.sort().toString()) {
+      console.log("Updated memcached server list to ", servers);
+      memcachedServers = servers;
 
-			//Disconnect an existing client
-			if (memcached)
-				await memcached.disconnect()
+      //Disconnect an existing client
+      if (memcached) await memcached.disconnect();
 
-			memcached = new MemcachePlus(memcachedServers);
-		}
-	} catch (e) {
-		console.log("Unable to get memcache servers (yet)")
-	}
+      memcached = new MemcachePlus(memcachedServers);
+    }
+  } catch (e) {
+    console.log("Unable to get memcache servers (yet)");
+  }
 }
 
 //Initially try to connect to the memcached servers, then each 5s update the list
-getMemcachedServersFromDns()
-setInterval(() => getMemcachedServersFromDns(), options.memcachedUpdateInterval)
+getMemcachedServersFromDns();
+setInterval(
+  () => getMemcachedServersFromDns(),
+  options.memcachedUpdateInterval
+);
 
 //Get data from cache if a cache exists yet
 async function getFromCache(key) {
-	if (!memcached) {
-		console.log(`No memcached instance available, memcachedServers = ${memcachedServers}`)
-		return null;
-	}
-	return await memcached.get(key);
+  if (!memcached) {
+    console.log(
+      `No memcached instance available, memcachedServers = ${memcachedServers}`
+    );
+    return null;
+  }
+  return await memcached.get(key);
 }
 
 // -------------------------------------------------------
@@ -115,30 +142,28 @@ async function getFromCache(key) {
 
 // Kafka connection
 const kafka = new Kafka({
-	clientId: options.kafkaClientId,
-	brokers: [options.kafkaBroker],
-	retry: {
-		retries: 0
-	}
-})
+  clientId: options.kafkaClientId,
+  brokers: [options.kafkaBroker],
+  retry: {
+    retries: 0,
+  },
+});
 
-const producer = kafka.producer()
+const producer = kafka.producer();
 
 // Send tracking message to Kafka
 async function sendTrackingMessage(data) {
-	//Ensure the producer is connected
-	await producer.connect()
+  //Ensure the producer is connected
+  await producer.connect();
 
-	//Send message
-	let result = await producer.send({
-		topic: options.kafkaTopicTracking,
-		messages: [
-			{ value: JSON.stringify(data) }
-		]
-	})
+  //Send message
+  let result = await producer.send({
+    topic: options.kafkaTopicTracking,
+    messages: [{ value: JSON.stringify(data) }],
+  });
 
-	console.log("Send result:", result)
-	return result
+  console.log("Send result:", result);
+  return result;
 }
 
 // -------------------------------------------------------
@@ -146,7 +171,7 @@ async function sendTrackingMessage(data) {
 // -------------------------------------------------------
 
 function sendResponse(res, html) {
-	res.send(`<!DOCTYPE html>
+  res.send(`<!DOCTYPE html>
 		<html lang="en">
 		<head>
 			<meta charset="UTF-8">
@@ -175,20 +200,8 @@ function sendResponse(res, html) {
 					</button>
 					<div class="collapse navbar-collapse" id="navbarContent">
 						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-							<li class="nav-item">
-								<a class="nav-link" href="#">Our Mission</a>
-							</li>
 							<li class="nav-item dropdown">
 								<a class="nav-link" href="/projects">Our Projects</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Publications</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Contact</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">GDPR</a>
 							</li>
 						</ul>
 					</div>
@@ -202,7 +215,7 @@ function sendResponse(res, html) {
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 		</body>
 	</html>
-	`)
+	`);
 }
 
 // -------------------------------------------------------
@@ -211,48 +224,57 @@ function sendResponse(res, html) {
 
 // Get list of projects (from cache or db)
 async function getProjects() {
-	const key = 'projects'
-	let cachedata = await getFromCache(key)
+  const key = "projects";
+  let cachedata = await getFromCache(key);
 
-	if (cachedata) {
-		console.log(`Cache hit for key=${key}, cachedata = `, cachedata)
-		return { result: cachedata, cached: true }
-	} else {
-		console.log(`Cache miss for key=${key}, querying database`)
-		const data = await executeQuery("SELECT id, title, description FROM projects", [])
-		if (data) {
-			let result = data
-				.map(row => ({ id: row?.[0], title: row?.[1], description: row?.[2] }));
-			console.log("Got result=", result, "storing in cache")
-			if (memcached)
-				await memcached.set(key, result, cacheTimeSecs);
-			return { result, cached: false }
-		} else {
-			throw "No projects data found"
-		}
-	}
+  if (cachedata) {
+    console.log(`Cache hit for key=${key}, cachedata = `, cachedata);
+    return { result: cachedata, cached: true };
+  } else {
+    console.log(`Cache miss for key=${key}, querying database`);
+    const data = await executeQuery(
+      "SELECT id, title, description FROM projects",
+      []
+    );
+    if (data) {
+      let result = data.map((row) => ({
+        id: row?.[0],
+        title: row?.[1],
+        description: row?.[2],
+      }));
+      console.log("Got result=", result, "storing in cache");
+      if (memcached) await memcached.set(key, result, cacheTimeSecs);
+      return { result, cached: false };
+    } else {
+      throw "No projects data found";
+    }
+  }
 }
 
 // Get popular projects (from db only)
 async function getPopular(maxCount) {
-	const query = "SELECT project, count FROM popular ORDER BY count DESC LIMIT ?"
-	return (await executeQuery(query, [maxCount]))
-		.map(row => ({ project: row?.[0], count: row?.[1] }))
+  const query =
+    "SELECT project, count FROM popular ORDER BY count DESC LIMIT ?";
+  return (await executeQuery(query, [maxCount])).map((row) => ({
+    project: row?.[0],
+    count: row?.[1],
+  }));
 }
 
 app.get("/", (req, res) => {
-	const topX = 6;
-	Promise.all([getProjects(), getPopular(topX)]).then(values => {
-		const projects = values[0]
-		const popular = values[1]
+  const topX = 6;
+  Promise.all([getProjects(), getPopular(topX)]).then((values) => {
+    const projects = values[0];
+    const popular = values[1];
 
-		let projectTable = []
-		projects.result.forEach(project => {
-			projectTable[project.id] = project;
-		});
+    let projectTable = [];
+    projects.result.forEach((project) => {
+      projectTable[project.id] = project;
+    });
 
-		const popularHtml = popular
-			.map(pop => `
+    const popularHtml = popular
+      .map(
+        (pop) => `
 				<div class="col-sm-4 mb-4">
 					<div class="card">
 						<div class="card-body">
@@ -262,10 +284,13 @@ app.get("/", (req, res) => {
 						</div>
 					</div>
 				</div>
-			`)
-			.join("\n")
+			`
+      )
+      .join("\n");
 
-		sendResponse(res, `
+    sendResponse(
+      res,
+      `
 			<h1>Big Data Research Institute</h1>
 
 			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
@@ -273,46 +298,54 @@ app.get("/", (req, res) => {
 			<div class="row">
 				${popularHtml}
 			</div>
-		`);
-	});
-})
+		`
+    );
+  });
+});
 
 app.get("/projects", (req, res) => {
-	const topX = 6;
-	Promise.all([getProjects(), getPopular(topX)]).then(values => {
-		const projects = values[0]
-		const popular = values[1]
+  const topX = 6;
+  Promise.all([getProjects(), getPopular(topX)]).then((values) => {
+    const projects = values[0];
+    const popular = values[1];
 
-		let projectTable = []
-		projects.result.forEach(project => {
-			projectTable[project.id] = project;
-		});
+    let projectTable = [];
+    projects.result.forEach((project) => {
+      projectTable[project.id] = project;
+    });
 
-		const popularHtml = popular
-			.map(pop => `
+    const popularHtml = popular
+      .map(
+        (pop) => `
 				<div class="card mb-4">
 					<div class="card-body">
-						<h5 class="card-title">${projectTable[pop.project].title} (${pop.count} views)</h5>
+						<h5 class="card-title">${projectTable[pop.project].title} (${
+          pop.count
+        } views)</h5>
 						<p class="card-text">${projectTable[pop.project].description}</p>
 						<a href='projects/${pop.project}'>Read more</a>
 					</div>
 				</div>
-			`)
-			.join("\n")
+			`
+      )
+      .join("\n");
 
-		const projectsHtml = projects.result
-			.map(m => `<p><a href='projects/${m.id}'>${m.title}</a> - ${m.description}</p>`)
-			.join("\n")
+    const projectsHtml = projects.result
+      .map(
+        (m) =>
+          `<p><a href='projects/${m.id}'>${m.title}</a> - ${m.description}</p>`
+      )
+      .join("\n");
 
-		const html = `
+    const html = `
 			<h1>Popular Projects</h1>
 			${popularHtml}
 
 			<h1>All Projects</h1>
 			${projectsHtml}
-		`
-		sendResponse(res, html)
-	});
+		`;
+    sendResponse(res, html);
+  });
 });
 
 // -------------------------------------------------------
@@ -320,51 +353,67 @@ app.get("/projects", (req, res) => {
 // -------------------------------------------------------
 
 async function getProject(project) {
-	const query = "SELECT title, description, text FROM projects WHERE id = ?"
-	const key = 'project_' + project
-	let cachedata = await getFromCache(key)
+  const query = "SELECT title, description, text FROM projects WHERE id = ?";
+  const key = "project_" + project;
+  let cachedata = await getFromCache(key);
 
-	if (cachedata) {
-		console.log(`Cache hit for key=${key}, cachedata = ${cachedata}`)
-		return { ...cachedata, cached: true }
-	} else {
-		console.log(`Cache miss for key=${key}, querying database`)
+  if (cachedata) {
+    console.log(`Cache hit for key=${key}, cachedata = ${cachedata}`);
+    return { ...cachedata, cached: true };
+  } else {
+    console.log(`Cache miss for key=${key}, querying database`);
 
-		let data = (await executeQuery(query, [project]))?.[0] // first entry
-		if (data) {
-			let result = { title: data?.[0], description: data?.[1], text: data?.[2] }
-			console.log(`Got result=${result}, storing in cache`)
-			if (memcached)
-				await memcached.set(key, result, cacheTimeSecs);
-			return { ...result, cached: false }
-		} else {
-			throw "No data found for this project"
-		}
-	}
+    let data = (await executeQuery(query, [project]))?.[0]; // first entry
+    if (data) {
+      let result = {
+        title: data?.[0],
+        description: data?.[1],
+        text: data?.[2],
+      };
+      console.log(`Got result=${result}, storing in cache`);
+      if (memcached) await memcached.set(key, result, cacheTimeSecs);
+      return { ...result, cached: false };
+    } else {
+      throw "No data found for this project";
+    }
+  }
 }
 
 app.get("/projects/:project", (req, res) => {
-	let project = req.params["project"]
+  let project = req.params["project"];
 
-	// Send the tracking message to Kafka
-	sendTrackingMessage({
-		project,
-		timestamp: Math.floor(new Date() / 1000)
-	}).then(() => console.log(`Sent project=${project} to kafka topic=${options.kafkaTopicTracking}`))
-		.catch(e => console.log("Error sending to kafka", e))
+  // Send the tracking message to Kafka
+  sendTrackingMessage({
+    project,
+    timestamp: Math.floor(new Date() / 1000),
+  })
+    .then(() =>
+      console.log(
+        `Sent project=${project} to kafka topic=${options.kafkaTopicTracking}`
+      )
+    )
+    .catch((e) => console.log("Error sending to kafka", e));
 
-	// Send reply to browser
-	getProject(project).then(data => {
-		let htmlText = data.text.split("\n").map(p => `<p>${p}</p>`).join("\n");
-		sendResponse(res, `
+  // Send reply to browser
+  getProject(project)
+    .then((data) => {
+      let htmlText = data.text
+        .split("\n")
+        .map((p) => `<p>${p}</p>`)
+        .join("\n");
+      sendResponse(
+        res,
+        `
 			<h1>${data.title}</h1>
 			<p>${data.description}<p>
 			<hr>
 			${htmlText}
-		`)
-	}).catch(err => {
-		sendResponse(res, `<h1>Error</h1><p>${err}</p>`, false)
-	})
+		`
+      );
+    })
+    .catch((err) => {
+      sendResponse(res, `<h1>Error</h1><p>${err}</p>`, false);
+    });
 });
 
 // -------------------------------------------------------
@@ -372,5 +421,5 @@ app.get("/projects/:project", (req, res) => {
 // -------------------------------------------------------
 
 app.listen(options.port, function () {
-	console.log("Node app is running at http://localhost:" + options.port)
+  console.log("Node app is running at http://localhost:" + options.port);
 });
